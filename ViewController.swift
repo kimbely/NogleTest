7b58b54ae23c8e6fcb626307bb56e756e073c581
KimbelyLiu <kimbely0320@gmail.com>
2023-09-09 09:23:43 +0800

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NogleTest
+//
+//  Created by 金梅劉 on 2023/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
